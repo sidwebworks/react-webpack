4a160a9d2b193f50a1e8121210bc684784ad668e
Sidharth Rathi <Xodedeveloper@gmail.com>
2022-07-13 00:10:56 +0530

    Clean up sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const config = {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
   },
